fe0ded7f17cdd2e981b6a1b29795ba726c4fb36f
student <student@Admins-iMac.local>
2019-10-14 15:50:54 -0500

    update

--- Tic_Tac_Toe/tictactoe.js
+++ Tic_Tac_Toe/tictactoe.js
@@ -1,67 +1,75 @@
 //Tic Tac Toe
 var newBoard;
-var user = 'X';
-var comp = 'O';
-var victory = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[6, 4, 2]];
-var squares = document.querySelectorAll('.cell');
-var tl=document.getElementById('0');
-var tm=document.getElementById('1');
-var tr=document.getElementById('2');
-var ml=document.getElementById('3');
-var mm=document.getElementById('4');
-var mr=document.getElementById('5');
-var bl=document.getElementById('6');
-var bm=document.getElementById('7');
-var br=document.getElementById('8');
-var table=document.getElementById('tbl');
-var squareArray=[0,1,2,3,4,5,6,7,8];
-var randomNum=randomNumber();
+var user = "X";
+var comp = "O";
+var victory = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+];
+var squares = document.querySelectorAll(".cell");
+var tl = document.getElementById("0");
+var tm = document.getElementById("1");
+var tr = document.getElementById("2");
+var ml = document.getElementById("3");
+var mm = document.getElementById("4");
+var mr = document.getElementById("5");
+var bl = document.getElementById("6");
+var bm = document.getElementById("7");
+var br = document.getElementById("8");
+var table = document.getElementById("tbl");
+var squareArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+var randomNum = randomNumber();
 var player;
 //New Game Function
-function newGame(){
-	document.querySelector('.end').style.display='none';
-	newBoard=Array.from(Array(9).keys());
-	for (var i=0;i<squares.length;i++){
-		squares[i].innerText='';
-        squares[i].addEventListener('click',nextTurn,false);
-	}
+function newGame() {
+  document.querySelector(".end").style.display = "none";
+  newBoard = Array.from(Array(9).keys());
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].innerText = "";
+    squares[i].addEventListener("click", nextTurn, false);
+  }
 }
 //Function alotting turns
-function nextTurn(box){
-	if (typeof newBoard[box.target.id]=='number'){
-		next(box.target.id, user);
-		if(!checkDraw())next(compChoice(),comp);
-	}
+function nextTurn(box) {
+  if (typeof newBoard[box.target.id] == "number") {
+    next(box.target.id, user);
+    if (!checkDraw()) next(compChoice(), comp);
+  }
 }
-function next(whichCell,player){
-	newBoard[whichCell]=player;
-	document.getElementById(whichCell).innerText=player;
-	let gameWon=checkWin(newBoard,player);
-	if(gameWon)gameOver(gameWon);
+function next(whichCell, player) {
+  newBoard[whichCell] = player;
+  document.getElementById(whichCell).innerText = player;
+  let gameWon = checkWin(newBoard, player);
+  if (gameWon) gameOver(gameWon);
 }
 //Check if game has been won by the player
-function checkWin(board,player){
-	let plays=board.reduce((a,e,i)=> 
-		(e===player)?a.concat(i):a,[]);
-	let gameWon=null;
-	for(let[index,win] of victory.entries()){
-		if(win.every(elem=>plays.indexOf(elem)>-1)){
-			gameWon={index:index,player:player};
-			break;
-		}
-	}
-	return gameWon;
+function checkWin(board, player) {
+  let plays = board.reduce((a, e, i) => (e === player ? a.concat(i) : a), []);
+  let gameWon = null;
+  for (let [index, win] of victory.entries()) {
+    if (win.every(elem => plays.indexOf(elem) > -1)) {
+      gameWon = { index: index, player: player };
+      break;
+    }
+  }
+  return gameWon;
 }
 //Function to check if there's a draw
-function checkDraw(){
-	if(emptyCell().length===0){
-		for(var i=0;i<squares.length;i++){
-			squares[i].removeEventListener('click',nextTurn,false);
-		}
-		nameWinner('Draw!');
-		return true;
-	}
-	return false;
+function checkDraw() {
+  if (emptyCell().length === 0) {
+    for (var i = 0; i < squares.length; i++) {
+      squares[i].removeEventListener("click", nextTurn, false);
+    }
+    nameWinner("Draw!");
+    return true;
+  }
+  return false;
 }
 /*
 var firstUser=1;
@@ -149,32 +157,32 @@ function update8(){
 */
 //Function that ends the game and declares whether user won or lost
 function gameOver(gameWon) {
-    for(var i=0;i<squares.length;i++){
-		squares[i].removeEventListener('click',nextTurn,false);
-	}
-	nameWinner(gameWon.player==user?'VICTORY!':'DEFEAT!');
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].removeEventListener("click", nextTurn, false);
+  }
+  nameWinner(gameWon.player == user ? "VICTORY!" : "DEFEAT!");
 }
 //Pushes the victory or defeat string to the end div
-function nameWinner(who){
-	document.querySelector('.end').style.display='block';
-	document.querySelector('.end .text').innerText=who;
+function nameWinner(who) {
+  document.querySelector(".end").style.display = "block";
+  document.querySelector(".end .text").innerText = who;
 }
 //Function to find empty cells
-function emptyCell(){
-	return newBoard.filter(s=>typeof s=='number');
+function emptyCell() {
+  return newBoard.filter(s => typeof s == "number");
 }
 //Function for a random number from 0 to 8
-function randomNumber(){
-	//console.log(Math.floor(Math.random()*8))
-	return Math.floor(Math.random()*8);
-  }
-//Function for the computer's choice
-function compChoice(){
-	//console.log(randomNum);
-	return emptyCell()[0];
-	//return emptyCell()[randomNum];
+function randomNumber() {
+  //console.log(Math.floor(Math.random()*8))
+  return Math.floor(Math.random() * 8);
 }
-//the 0 moves from 0-8 in order, 
+//Function for the computer's choice
+function compChoice() {
+  //console.log(randomNum);
+  return emptyCell()[0];
+  //return emptyCell()[randomNum];
+}
+//the 0 moves from 0-8 in order,
 //the randomNum moves randomly but breaks sometimes, reload page
 //swap commented out to change
 newGame();

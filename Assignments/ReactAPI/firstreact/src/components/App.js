1f53b6833b238b3114feb550ed1e3bb30e7b4254
student <student@Admins-iMac.local>
2019-10-24 17:00:16 -0500

    Thurs

--- Assignments/ReactAPI/firstreact/src/components/App.js
+++ Assignments/ReactAPI/firstreact/src/components/App.js
@@ -1,44 +1,53 @@
-import React, { Component } from 'react';
-import './styles.module.css';
-import 'react-bootstrap'
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            items: [],
-            isLoaded: false,
-        }
-    }
-    componentDidMount() {
-        fetch('https://ghibliapi.herokuapp.com/films')
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                this.setState({
-                    isLoaded: true,
-                    items: data,
-                })
-            });
-    }
-    render() {
-        var { isLoaded, items } = this.state;
-        if (!isLoaded) {
-            return 'Все счастли́вые се́мьи похо́жи друг на дру́га, ка́ждая несчастли́вая семья́ несчастли́ва по-сво́ему.'
-        } else {
-            return (
-                <div className="card" style={{width:"18rem",border:" 1px solid black"}} align="center">
-                    <div className="card-body">
-                        {items.map(item => (
-                            <React.Fragment>
-                                <h4 key={item.id} className="card-title">{item.title}</h4>
-                                <p key={item.id} className="card-text">{item.description}</p>
-                                <hr></hr>
-                            </React.Fragment>
-                        ))}
-                    </div>
-                </div>
+import React, { Component } from "react";
+import "./styles.module.css";
+import "react-bootstrap";
 
-                /*<div className="App">
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false
+    };
+  }
+  componentDidMount() {
+    fetch("https://ghibliapi.herokuapp.com/films")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          isLoaded: true,
+          items: data
+        });
+      });
+  }
+  render() {
+    var { isLoaded, items } = this.state;
+    if (!isLoaded) {
+      return "Все счастли́вые се́мьи похо́жи друг на дру́га, ка́ждая несчастли́вая семья́ несчастли́ва по-сво́ему.";
+    } else {
+      return (
+        <div
+          className="card"
+          style={{ width: "18rem", border: " 1px solid black" }}
+          align="center"
+        >
+          <div className="card-body">
+            {items.map(item => (
+              <React.Fragment>
+                <h4 key={item.id} className="card-title">
+                  {item.title}
+                </h4>
+                <p key={item.id} className="card-text">
+                  {item.description}
+                </p>
+                <hr></hr>
+              </React.Fragment>
+            ))}
+          </div>
+        </div>
+
+        /*<div className="App">
                  <ul>
                      {items.map(item=>(
                          <li key={item.id}>
@@ -49,8 +58,8 @@ class App extends Component {
                  </ul>
                  </div>
              */
-            )
-        };
+      );
     }
+  }
 }
 export default App;
--- Assignments/ReactN/reactnative/App.js
+++ Assignments/ReactN/reactnative/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View, ScrollView, StatusBar } from "react-native";
 import "react-bootstrap";
 import "./champions.json";
+import "./assets/champs.json";
+const champs = "./assets/champs.json";
 
 export default class App extends Component {
   constructor(props) {
@@ -12,62 +14,78 @@ export default class App extends Component {
     };
   }
   componentDidMount() {
-    fetch("./champions.json")
+    fetch('https://ghibliapi.herokuapp.com/films')
+    // fetch(".assets/champs.json")
+      // fetch(
+      //   "https://74f992c9-3fa4-4d14-bc7f-6d7572075e9f.mock.pstmn.io/champions"
+      // )
       .then(response => response.json())
       .then(data => {
         this.setState({
           isLoaded: true,
           items: data
-        });
+        })
       });
   }
   render() {
+    // var arr = [];
+    // console.log(Object.keys(champs));
+    // Object.keys(champs).forEach(function(key) {
+    //   arr.push(champs[key]);
+    // });
+    for(let item in items){
+      console.log(item);
     var { isLoaded, items } = this.state;
     if (!isLoaded) {
       return (
-        <Text>
+        <Text style={{ padding: "10%" }}>
           Все счастли́вые се́мьи похо́жи друг на дру́га, ка́ждая несчастли́вая семья́
           несчастли́ва по-сво́ему.
         </Text>
       );
     } else {
       return (
-        // <StatusBar hidden={true}>
+        //<StatusBar hidden={true}>
         <ScrollView>
           <View style={styles.container}>
             <React.Fragment>
-              {items.map(item => (
-                <React.Fragment>
-                  <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
-                    <Text
-                      h4
-                      key={item.id}
-                      style={{
-                        flexDirection: "row",
-                        flexWrap: "wrap",
-                        color: "#e6dada"
-                      }}
-                    >
-                      {this.item.name}
-                    </Text>
-                  </View>
-                  <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
-                    <Text p key={item.id} style={styles.cardtext}>
-                      {this.item.blurb}
-                    </Text>
-                    <View
-                      style={{
-                        borderBottomColor: "black",
-                        borderBottomWidth: 1
-                      }}
-                    />
-                  </View>
-                </React.Fragment>
-              ))}
+              {items.map(
+                key => (
+                  console.log(key),
+                  (
+                    <React.Fragment>
+                      <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+                        <Text
+                          h4
+                          key={key.id}
+                          style={{
+                            flexDirection: "row",
+                            flexWrap: "wrap",
+                            color: "#e6dada"
+                          }}
+                        >
+                          {key.name}
+                        </Text>
+                      </View>
+                      <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+                        <Text p key={key.key} style={styles.cardtext}>
+                          {key.blurb}
+                        </Text>
+                        <View
+                          style={{
+                            borderBottomColor: "black",
+                            borderBottomWidth: 1
+                          }}
+                        />
+                      </View>
+                    </React.Fragment>
+                  )
+                )
+              )}
             </React.Fragment>
           </View>
         </ScrollView>
-        // </StatusBar>
+        //</StatusBar>
       );
     }
   }

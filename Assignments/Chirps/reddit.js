883a6feb85ea41e6cbb51b06fa7acce78bce35f4
student <student@Admins-iMac.local>
2019-11-06 10:38:58 -0600

    Wed

--- Assignments/Chirps/reddit.js
+++ Assignments/Chirps/reddit.js
@@ -1,20 +1,39 @@
 const fs = require("fs");
 const rp = require("request-promise");
 
-exports.rdt = async function() {
-  const options = {
+async function rdt() {
+  await rp({
     method: "GET",
     uri: "https://www.reddit.com/r/popular.json",
+    headers: {
+      "User-Agent": "Request-Promise"
+    },
     json: true
-  };
-
-  rp(options)
-    .then(function(data) {
+  })
+    .then(body => {
+      // console.log(body);
       let redditArr = [];
-      redditArr.push(data),
-        fs.writeFile("./popular-articles.json", JSON.stringify(redditArr)),
+      body.data.children.forEach(stuff => {
+        let title = stuff.data.title;
+        let author = stuff.data.author;
+        let url = stuff.data.url;
+
+        redditArr.push({ title, author, url }),
+          fs.writeFile(
+            "./popular-articles.json",
+            JSON.stringify(redditArr),
+            err => {
+              if (err) {
+                throw err;
+              }
+            }
+          );
         console.log(redditArr);
+      });
     })
-    .catch(handleError);
+    .catch(function(err) {
+      console.log("err");
+    });
 }
 
+module.exports.rdt = rdt;

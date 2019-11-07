3a84e74ef213c94eeaf4eaf6712f3fc17826b0df
student <student@Admins-iMac.local>
2019-11-07 09:34:01 -0600

    Thurs

--- Assignments/expressassign/server.js
+++ Assignments/expressassign/server.js
@@ -10,14 +10,15 @@ app.use(
   })
 );
 
-app.use('/', function (req, res, next) {
-  console.log('Request URL:', req.url)
-  next()
-})
+app.use("/", function(req, res, next) {
+  console.log("Request URL:", req.url);
+  next();
+});
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/public/index.html");
-  });
+  console.log("Hello from the web server side...");
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

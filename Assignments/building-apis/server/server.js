4a7a24766938fc5a1ad871e0e412a4694abdba5f
student <student@Admins-iMac.local>
2019-11-12 10:40:01 -0600

    Tues

--- Assignments/building-apis/server/server.js
+++ Assignments/building-apis/server/server.js
@@ -7,7 +7,7 @@ let app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../../client")));
+app.use(express.static(path.join(__dirname, "../client")));
 
 app.use("/api", apiRouter);
 

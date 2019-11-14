5a4a3de4891bae66273d272782503a49ce4ccae9
student <student@Admins-iMac.local>
2019-11-14 15:14:10 -0600

    Thurs

---
+++ pypassword/passwords.py
@@ -0,0 +1,23 @@
+import random
+
+print('''
+==================
+Password Generator
+==================
+''')
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*().,?0123456789'
+
+number = input('How many passwords?')
+number = int(number)
+
+length = input('How long do you want your password?')
+length = int(length)
+
+print('\nhere are your passwords:')
+
+for pwd in range(number):
+  password = ''
+  for c in range(length):
+    password += random.choice(chars)
+  print(password)

b27ac153a60384ef903f0e0166d7c41ff32a7b8a
student <student@Admins-iMac.local>
2019-11-22 09:15:49 -0600

    working

--- Hackathon/app.js
+++ Hackathon/app.js
@@ -3,19 +3,18 @@ const path = require('path');
 var bodyParser = require("body-parser");
 var app = express();
 
-// app.use("/client", express.static("/client"));
+app.use("/client", express.static("client"));
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
 );
-app.use(express.static(path.join(__dirname + '/client')));
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/login.html");
 });
 app.get("/dash", function(req, res) {
-  res.sendFile(__dirname + "/client/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 const PORT = process.env.PORT || 3000;

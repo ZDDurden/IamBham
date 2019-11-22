a61a7f2fd586db59342d3135876b09d18942cf03
Zach <zddurden@crimson.ua.edu>
2019-11-21 20:58:12 -0600

    Update

--- Hackathon/app.js
+++ Hackathon/app.js
@@ -1,20 +1,22 @@
 var express = require("express");
+const path = require('path');
 var bodyParser = require("body-parser");
 var app = express();
 
-app.use("/assets", express.static("assets"));
+// app.use("/client", express.static("/client"));
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
 );
+app.use(express.static(path.join(__dirname + '/client')));
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/login.html");
 });
-app.get("/snake", function(req, res) {
-  res.sendFile(__dirname + "/views/snake.html");
-
+app.get("/dash", function(req, res) {
+  res.sendFile(__dirname + "/client/index.html");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
